77c76156af7d58a6c562d2ad16ef6df95668ae9a
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-11-24 11:41:44 +0900

    テキストフィールド入力時のキーボードレイアウト崩れを解消 #22

--- app/src/main/java/com/android/bookmanager_kotlin/fragment/BookListRecyclerViewAdapter.kt
+++ app/src/main/java/com/android/bookmanager_kotlin/fragment/BookListRecyclerViewAdapter.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
 import com.android.bookmanager_kotlin.R
 import com.android.bookmanager_kotlin.model.Book
