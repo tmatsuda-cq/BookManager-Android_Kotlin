23433537bc1505c6de2c38b770bd4e29fc08bc68
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2020-12-27 11:35:57 +0900

    書籍追加、編集画面に画像取得表示処理を仮実装（修正要）

--- app/src/main/java/com/android/bookmanager_kotlin/activity/AddBookActivity.kt
+++ app/src/main/java/com/android/bookmanager_kotlin/activity/AddBookActivity.kt
@@ -1,11 +1,18 @@
 package com.android.bookmanager_kotlin.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import com.android.bookmanager_kotlin.R
 import com.android.bookmanager_kotlin.util.DatePickerUtils.showDatePicker
 import kotlinx.android.synthetic.main.activity_add_book.*
+import java.lang.Exception
 
 class AddBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +21,71 @@ class AddBookActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        bt_add_book_image.setOnClickListener {
+            // 推奨
+            //selectBookImage()
+
+            // TODO: 非推奨のため削除
+            showImageStrage()
+        }
+
         et_add_book_purchase_date.setOnClickListener {
             showDatePicker(this, et_add_book_purchase_date)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        if (requestCode == 42) {
+            try {
+                data?.data?.also { uri ->
+                    val inputStream = contentResolver?.openInputStream(uri)
+                    val image = BitmapFactory.decodeStream(inputStream)
+                    iv_add_book_image.setImageBitmap(image)
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this, "画像選択処理に失敗しました", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun showImageStrage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, 42)
+    }
+
+    private fun selectBookImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+            if (result?.resultCode == Activity.RESULT_OK) {
+                result.data?.let { data: Intent ->
+                    try {
+                        data?.data?.also { uri ->
+                            val inputStream = contentResolver?.openInputStream(uri)
+                            val image = BitmapFactory.decodeStream(inputStream)
+                            iv_add_book_image.setImageBitmap(image)
+                        }
+                    } catch (e: Exception) {
+                        Toast.makeText(this, "画像選択処理に失敗しました", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+        launcher.launch(intent)
+    }
+
 
     // アクションバー戻るボタンクリック処理
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/android/bookmanager_kotlin/fragment/EditBookFragment.kt
+++ app/src/main/java/com/android/bookmanager_kotlin/fragment/EditBookFragment.kt
@@ -1,17 +1,24 @@
 package com.android.bookmanager_kotlin.fragment
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.setFragmentResultListener
 import com.android.bookmanager_kotlin.R
 import com.android.bookmanager_kotlin.util.DatePickerUtils.showDatePicker
 import com.android.bookmanager_kotlin.util.FragmentUtils
 import kotlinx.android.synthetic.main.fragment_edit_book.*
+import java.lang.Exception
 
 class EditBookFragment : Fragment() {
 
@@ -39,10 +46,39 @@ class EditBookFragment : Fragment() {
     override fun onStart() {
         super.onStart()
 
+        // 画像取得処理
+        bt_edit_book_image.setOnClickListener {
+            selectBookImage()
+        }
+
         // onCreateView()で呼ぶとnullポになる
         et_edit_book_purchase_date.setOnClickListener {
             showDatePicker(requireContext(), et_edit_book_purchase_date)
         }
+    }
+
+    private fun selectBookImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+
+        val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult? ->
+            if (result?.resultCode == Activity.RESULT_OK) {
+                result.data?.let { data: Intent ->
+                    try {
+                        data?.data?.also { uri ->
+                            val inputStream = activity?.contentResolver?.openInputStream(uri)
+                            val image = BitmapFactory.decodeStream(inputStream)
+                            iv_edit_book_image.setImageBitmap(image)
+                        }
+                    } catch (e: Exception) {
+                        Toast.makeText(activity?.applicationContext, "画像選択処理に失敗しました", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+        launcher.launch(intent)
     }
 
     // アクションバー戻るボタンクリックでフラグメント切り替え
